b05318b3c926d3aa030254b9dc61ae720054de7b
Tian Qiu <t.qiu0620231@arts.ac.uk>
2023-11-21 13:31:24 +0000

    11.21

--- STEM-Week-7-Video-Face-Filter.py
+++ STEM-Week-7-Video-Face-Filter.py
@@ -25,11 +25,24 @@ class MySketch:
             for face_x, face_y, face_w, face_h in faces:
             #Draw rectangle around face
             ##ADD YOUR OWN CODE IN HERE
-                cv2.circle(camera_feed,
-                            (face_x+face_w//2,face_y+face_h//2),
-                            face_w//2,255,-1)
-            
-            an.canvas = camera_feed
+            ##ADD YOUR OWN CODE IN HERE
+                # Draw sunglasses shape (here, a rectangle for demonstration purposes)
+                glass_width = int(face_w * 0.6)
+                glass_height = int(face_h * 0.15)
+                glass_x = face_x + int((face_w - glass_width) / 2)
+                glass_y = face_y + int(face_h * 0.35)
+                
+                # Draw the frame of the sunglasses (curved shape)
+                cv2.ellipse(camera_feed, (face_x + face_w // 2, glass_y + glass_height // 2), 
+                            (glass_width // 2, glass_height // 2), 0, 0, 180, (0, 0, 0), -1)
+                
+                # Draw the left lens of the sunglasses (ellipse)
+                cv2.ellipse(camera_feed, (glass_x + int(glass_width * 0.25), glass_y + glass_height // 2), 
+                            (int(glass_width * 0.2), int(glass_height * 0.8)), 0, 0, 360, (0, 0, 0), -1)
+                
+                # Draw the right lens of the sunglasses (ellipse)
+                cv2.ellipse(camera_feed, (glass_x + int(glass_width * 0.75), glass_y + glass_height // 2), 
+                            (int(glass_width * 0.2), int(glass_height * 0.8)), 0, 0, 360, (0, 0, 0), -1)
         
 MySketch()          
 
